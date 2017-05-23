e28ada7b8df5e8859f8f65a8afc91c0f0ce6ca49
Amandeep <uncommon.aman@gmail.com>
2017-05-23 17:36:58 +0530

    add photo gallery

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,6 +11,14 @@ $(function() {
 	 $(".biglink").fitText(1.5);
 
 	 $('textarea').autosize();
+
+    // $('.fadein img:gt(0)').hide();
+    // setInterval(function(){
+    //   $('.fadein :first-child').fadeOut()
+    //      .next('img').fadeIn()
+    //      .end().appendTo('.fadein');}, 
+    //   3000);
+
 });
 
 function changeFrame() {
